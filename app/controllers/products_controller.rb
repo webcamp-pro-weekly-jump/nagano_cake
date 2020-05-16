cf427469f1db44cf6993e1298835bc186503423e
Anju-Komaki <upfmdb1124ad@gmail.com>
2020-05-16 07:00:27 +0000

    [add]view:products,cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,14 @@
 class ProductsController < ApplicationController
+    before_action :authenticate_member!
+
+    #ジャンル機能できたらif~elseで反映させます
+	def index
+		@products = Product.all
+	end
+
+	def show
+		@products = Product.find(params[:id])
+		@cart_item = CartItem.new
+	end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+	attachment :image
+	
+	has_many: :cart_item dependent: :destroy
+
 end
