0e7d2a0cd6e5cd0cc05b8d3e7b9aa6f34162891e
Hiroaki-Satoh <yurayuraclover.liebe@gmail.com>
2020-05-17 14:31:42 +0900

    Update OrdersController

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,31 @@
 class OrdersController < ApplicationController
+
+	def new
+	end
+
+	def index
+	end
+
+	def show
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def order_show
+	end
+
+	def success
+	end
+
+	private
+	def order_params
+		params.require(:order).permit()
+	end
 end
