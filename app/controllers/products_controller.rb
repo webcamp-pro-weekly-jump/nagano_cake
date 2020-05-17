cfeb063fbff96af3c56858791f8de2682df4c93a
Anju-Komaki <upfmdb1124ad@gmail.com>
2020-05-17 11:29:08 +0000

    [add]productsindex-genre search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,17 @@
 class ProductsController < ApplicationController
     before_action :authenticate_member!
 
-    #ジャンル機能できたらif~elseで反映させます
 	def index
-		@products = Product.all
+		@genres = Genre.all
+		if params[:genre_id]
+			@genre = genre.find(params[:genre_id])
+			@products = product.where(genre_id: @genre.id)
+	    else
+	        @products = Product.all
+	    end
 	end
 
+　　#商品詳細→カートに入れる
 	def show
 		@product = Product.find(params[:id])
 		@cart_item = CartItem.new
