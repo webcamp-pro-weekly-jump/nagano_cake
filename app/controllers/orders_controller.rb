e3383ab41536ef39c0bf785f176ad57044f67a0f
Hiroaki-Satoh <yurayuraclover.liebe@gmail.com>
2020-05-17 16:15:38 +0900

    Update OrdersController

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,21 @@
 class OrdersController < ApplicationController
+	# ログイン済み会員のみにアクセス許可
+	before_action :authenticate_member!
+	before_action :set_member
+	before_action :order_new, only:[:new, :show, :order_show]
 
 	def new
 	end
 
 	def index
+		@orders = Order.all
 	end
 
 	def show
+		@order = Order.find(params[:id])
 	end
 
 	def create
-	end
-
-	def edit
-	end
-
-	def update
 	end
 
 	def order_show
@@ -25,7 +25,20 @@ class OrdersController < ApplicationController
 	end
 
 	private
+	def set_member
+		@menber = current_member
+	end
+
+	def order_new
+		@order = Order.new
+	end
+
 	def order_params
-		params.require(:order).permit()
+		params.require(:order).permit(:name, :address, :postal_code,
+									  :pay_method, :postage, :all_price,
+									  :order_status, :create_at,
+			  order_item_attributes: [:order_id,:product_id, :quantity,
+			  						  :tax_price,:making_status]
+			  						  )
 	end
 end
