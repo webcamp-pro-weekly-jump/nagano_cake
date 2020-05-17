7aebae287dba4c0249a96b6126dd2a8fa346197e
Anju-Komaki <upfmdb1124ad@gmail.com>
2020-05-17 05:06:03 +0000

    [add]mypage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  	def after_sign_in_path_for(resource)
+    	root_path(resource)
+  	end
+  	def after_sign_out_path_for(resource)
+  		root_path(resource)
+  	end
+
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	 def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :fist_name, :family_name_kana, :first_name_kana, :postal_code, :address, :tel])
+    end
 end
