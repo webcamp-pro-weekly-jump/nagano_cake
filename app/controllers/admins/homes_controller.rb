098f7eee8e54c223242293f02bd6ac2f688f00db
vagrant <vagrant@localhost.localdomain>
2020-05-21 12:41:54 +0000

    カウント修正

--- app/controllers/admins/homes_controller.rb
+++ app/controllers/admins/homes_controller.rb
@@ -1,7 +1,7 @@
 class Admins::HomesController < ApplicationController
 	before_action :authenticate_admin!
 	def top
-	 @orders = Order.all
+	 @orders = Order.where("created_at LIKE ?", "#{Time.zone.today}%")
 	 @order = Order.where(params[:id])
 	end
 
