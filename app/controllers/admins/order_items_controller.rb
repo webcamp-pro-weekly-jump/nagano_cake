6359655185b3aafce50a3efdb8bf395cc9a26254
TAKERU ARAKAWA <takeruarakawa@TAKERUnoMacBook-Air.local>
2020-05-21 20:58:17 +0900

    修正

--- app/controllers/admins/order_items_controller.rb
+++ app/controllers/admins/order_items_controller.rb
@@ -1,10 +1,11 @@
 class Admins::OrderItemsController < ApplicationController
 	before_action :authenticate_admin!
 	def update
-		@order = Order.find(params[:id])
+		# @order = Order.find(params[:id])
 		@order_item = OrderItem.find(params[:id])
-		@order_item.save(order_item_params)
-		redirect_to admins_order_path(@order), notice: "製作ステータスを更新しました！"
+		@order_item.update(order_item_params)
+		redirect_to admins_order_path(@order_item.order_id), notice: "製作ステータスを更新しました！"
+		#@order_item.order_idで注文商品の含まれている注文にリダイレクトする。
 	end
 
 	private
