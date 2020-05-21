bf58d70aa1e69a9b8da9cb6241c5297288020398
TAKERU ARAKAWA <takeruarakawa@TAKERUnoMacBook-Air.local>
2020-05-21 16:42:00 +0900

    [view_controller]

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,4 +10,12 @@ class Order < ApplicationRecord
 	# 送り先住所/郵便番号の空データの禁止
 	validates :address, presence: true
 	validates :postal_code, presence: true
+
+	enum order_status:{
+		"入金待ち": 0,
+		"入金確認": 1,
+		"製作中": 2,
+		"発送準備中": 3,
+		"発送済": 4
+	}
 end
